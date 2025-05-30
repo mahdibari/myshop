3c36c29d1dee59e6a6b2d40a7431b88669f298ea
mahdibari <mehdi.eblis.82@gmail.com>
2025-05-30 22:41:19 +0200

    Updated

--- components/Header.tsx
+++ components/Header.tsx
@@ -40,7 +40,7 @@ export default function Header() {
           {/* بخش راست: لوگو + لینک‌ها در دسکتاپ */}
           <div className="flex items-center gap-6">
             <Link href="/" className="text-2xl font-bold text-pink-600 whitespace-nowrap">
-              Persian Beauty Rose
+              کد اینماد برای تایید شدن : 50314374
             </Link>
 
             {/* لینک‌ها در حالت دسکتاپ */}
